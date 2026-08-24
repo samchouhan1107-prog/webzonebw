532bfdc1ac894c19c7fe7f9a0318da4145da1e19
Sameer Chouhan <sam.chouhan32@gmail.com>
2026-08-24 13:37:40 +0530

    describe all changes

--- root/sw.js
+++ root/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11495810
+    "zoneId": 11642202
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
